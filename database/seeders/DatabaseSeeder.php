c7a477ff1e4889aae2e761df631bbf1afbb649a6
Adam Thomas <thomasadam83@hotmail.com>
2022-07-18 23:55:28 -0400

    Removed models from seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,16 +2,6 @@
 
 namespace Database\Seeders;
 
-use App\Models\Article;
-use App\Models\ArticleType;
-use App\Models\Evaluation;
-use App\Models\Meme;
-use App\Models\Page;
-use App\Models\Social;
-use App\Models\Tag;
-use App\Models\Tool;
-use App\Models\ToolType;
-use App\Models\Topic;
 use App\Models\User;
 
 use Illuminate\Database\Seeder;
@@ -30,7 +20,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        
+
         // \App\Models\User::factory(10)->create();
 
         // Schema::disableForeignKeyConstraints();
