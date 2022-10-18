f276c4d53db0d873cac9aad4b372d133cc0dbb12
Adam Thomas <thomasadam83@hotmail.com>
2022-10-18 09:50:16 -0400

    Changed CodeAdam to BrickMMO

--- resources/views/layout/console.blade.php
+++ resources/views/layout/console.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>CodeAdam Website Admin</title>
+        <title>BrickMMO Website Admin</title>
 
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lora">
         <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
