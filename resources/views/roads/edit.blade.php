1563d4765b48462cbe8269219a552551b6c70610
Adam Thomas <thomasadam83@hotmail.com>
2022-12-04 18:03:06 -0500

    Added road edit form

--- resources/views/roads/edit.blade.php
+++ resources/views/roads/edit.blade.php
@@ -4,21 +4,19 @@
 
 <section class="w3-padding ca-container-small">
 
-    @include ('layout.title', ['title' => 'Edit Map'])
+    @include ('layout.title', ['title' => 'Edit Road'])
 
-    @include ('layout.breadcrumbs', ['links' => ['Manage Maps' => '/maps/list'], 'title' => 'Edit Map: '.$map->title])
+    @include ('layout.breadcrumbs', ['links' => ['Manage Roads' => '/roads/list'], 'title' => 'Edit Road: '.$road->title])
 
-    <form method="post" action="/maps/edit/{{$map->id}}" novalidate class="w3-margin-bottom" autocomplete="off">
+    <form method="post" action="/roads/edit/{{$road->id}}" novalidate class="w3-margin-bottom" autocomplete="off">
 
         @csrf
 
-        @include ('layout.forms.text', ['name' => 'title', 'value' => $map->title])
+        @include ('layout.forms.text', ['name' => 'title', 'value' => $road->title])
 
-        @include ('layout.forms.text', ['name' => 'width', 'value' => $map->width])
+        @include ('layout.forms.select', ['name' => 'map_id', 'label' => 'Map', 'options' => $maps, 'type' => 'table', 'selected' => $road->map_id])
 
-        @include ('layout.forms.text', ['name' => 'height', 'value' => $map->height])
-
-        @include ('layout.forms.button', ['label' => 'Edit Map'])
+        @include ('layout.forms.button', ['label' => 'Edit Road'])
 
     </form>
 
